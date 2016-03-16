7b599593c4c0e7a05e71a24699efcd5540f25bb0
jed1337 <j.caychingco@Yahoo.com>
2016-03-16 18:50:16 +0800

    Added basic movement (Not yet mobile)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpHeight;
+	private Rigidbody2D rigi;
+
+	private void awake() {
+	}
+	
+	// Use this for initialization
+	void Start () {
+		rigi = GetComponent<Rigidbody2D>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+										//0 horizontal move, jump height vertical move
+			rigi.velocity = new Vector2(rigi.velocity.x, jumpHeight);
+		}
+
+		if (Input.GetKey(KeyCode.D)) {
+										//moveSpeed horizontal, gets current vertical velocity for the y
+										//Setting the vertical move to 0 will not allow the user to
+											//Jump if D is held
+			rigi.velocity = new Vector2(moveSpeed, rigi.velocity.y);
+		}
+
+		if (Input.GetKey(KeyCode.A)) {
+			rigi.velocity = new Vector2(-moveSpeed, rigi.velocity.y);
+		}
+	}
+}
