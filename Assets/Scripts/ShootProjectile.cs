4155597732831dc499639ff8dc0f5c03bbc6bcf2
jed1337 <j.caychingco@Yahoo.com>
2016-03-26 02:01:23 +0800

    Added fire rate to enemy projectile

--- Assets/Scripts/ShootProjectile.cs
+++ Assets/Scripts/ShootProjectile.cs
@@ -3,17 +3,22 @@ using System.Collections;
 
 public class ShootProjectile : MonoBehaviour {
 	public float playerRange;
+	public float fireRate;
+
 	public GameObject projectile;
 	public PlayerController player;
 	public Transform launchPoint;
 
-	// Use this for initialization
+	private float shotCounter;
+
 	void Start () {
-		player = FindObjectOfType<PlayerController>();		
+		player = FindObjectOfType<PlayerController>();
+		shotCounter = fireRate;
 	}
 
-	// Update is called once per frame
 	void Update() {
+		shotCounter -= Time.deltaTime;
+
 		Vector3 playerPosition = player.transform.position;
 		Vector3 projectilePosition = transform.position;
 
@@ -21,16 +26,18 @@ public class ShootProjectile : MonoBehaviour {
 		Vector3 end = new Vector3(projectilePosition.x + playerRange, projectilePosition.y, projectilePosition.z);
 		Debug.DrawLine(start, end, Color.magenta);
 
-		//If the player is within range, fire at him.
-		if((transform.localScale.x > 0
-			&& playerPosition.x < projectilePosition.x
-			&& playerPosition.x > projectilePosition.x - playerRange)
-			||
-			(transform.localScale.x > 0
-			&& playerPosition.x > projectilePosition.x
-			&& playerPosition.x < projectilePosition.x + playerRange)){
+		//Only fire if the enemy has reloaded
+		if (shotCounter < 0 && transform.localScale.x > 0) {
+			//If the player is within range, fire at him.
+			if (( playerPosition.x < projectilePosition.x	//Fire at the left
+				&& playerPosition.x > projectilePosition.x - playerRange)
+				||
+				(	playerPosition.x > projectilePosition.x	//Fire at the right
+				&& playerPosition.x < projectilePosition.x + playerRange)) {
 
-			Instantiate(projectile, launchPoint.position, launchPoint.rotation);
+				Instantiate(projectile, launchPoint.position, launchPoint.rotation);
+			}
+			shotCounter = fireRate;
 		}
 	}
 }
