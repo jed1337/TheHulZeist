88b080c3ac0b8bfb1781740447851b7889ea6e0d
monmilo9 <monzonmilo@yahoo.com>
2016-04-03 03:02:12 +0800

    a

---
+++ Assets/Scripts/Bounce.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bounce : MonoBehaviour {
+
+	public LayerMask collisionMask;
+	public Transform blade;
+
+	private float speed = 15;
+	private float rotSpeed = 800;
+
+	void Update() {
+		//Move the projectile forward
+		transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+		//Rotate it
+		blade.Rotate(Vector3.up * Time.deltaTime * rotSpeed);
+
+		Ray ray = new Ray(transform.position, transform.forward);
+		RaycastHit hit;
+
+		if (Physics.Raycast(ray, out hit, Time.deltaTime * speed + .1f, collisionMask)){
+			Vector3 reflectDir = Vector3.Reflect(ray.direction, hit.normal);
+			float rot = 90 - Mathf.Atan2(reflectDir.z, reflectDir.x) * Mathf.Rad2Deg;
+			transform.eulerAngles = new Vector3(0, rot, 0);
+		}
+	}
+}
--- Assets/Scripts/RepeatSpriteBoundary.cs
+++ Assets/Scripts/RepeatSpriteBoundary.cs
@@ -6,29 +6,29 @@ public class RepeatSpriteBoundary : MonoBehaviour {
 	SpriteRenderer spriteRenderer;
 	void Awake() {
 
-		spriteRenderer = GetComponent<SpriteRenderer>();
-		if (!SpritePivotAlignment.GetSpriteAlignment(gameObject).Equals(SpriteAlignment.TopRight)) {
-			Debug.LogError("You forgot change the sprite pivot to Top Right.");
-		}
-		Vector2 spriteSize = new Vector2(spriteRenderer.bounds.size.x / transform.localScale.x, spriteRenderer.bounds.size.y / transform.localScale.y);
+		//spriteRenderer = GetComponent<SpriteRenderer>();
+		//if (!SpritePivotAlignment.GetSpriteAlignment(gameObject).Equals(SpriteAlignment.TopRight)) {
+		//	Debug.LogError("You forgot change the sprite pivot to Top Right.");
+		//}
+		//Vector2 spriteSize = new Vector2(spriteRenderer.bounds.size.x / transform.localScale.x, spriteRenderer.bounds.size.y / transform.localScale.y);
 
-		GameObject childPrefab = new GameObject();
+		//GameObject childPrefab = new GameObject();
 
-		SpriteRenderer childSprite = childPrefab.AddComponent<SpriteRenderer>();
-		childPrefab.transform.position = transform.position;
-		childSprite.sprite = spriteRenderer.sprite;
+		//SpriteRenderer childSprite = childPrefab.AddComponent<SpriteRenderer>();
+		//childPrefab.transform.position = transform.position;
+		//childSprite.sprite = spriteRenderer.sprite;
 
-		GameObject child;
-		for (int i = 0, h = (int)Mathf.Round(spriteRenderer.bounds.size.y); i * spriteSize.y < h; i++) {
-			for (int j = 0, w = (int)Mathf.Round(spriteRenderer.bounds.size.x); j * spriteSize.x < w; j++) {
-				child = Instantiate(childPrefab) as GameObject;
-				child.transform.position = transform.position - (new Vector3(spriteSize.x * j, spriteSize.y * i, 0));
-				child.transform.parent = transform;
-			}
-		}
+		//GameObject child;
+		//for (int i = 0, h = (int)Mathf.Round(spriteRenderer.bounds.size.y); i * spriteSize.y < h; i++) {
+		//	for (int j = 0, w = (int)Mathf.Round(spriteRenderer.bounds.size.x); j * spriteSize.x < w; j++) {
+		//		child = Instantiate(childPrefab) as GameObject;
+		//		child.transform.position = transform.position - (new Vector3(spriteSize.x * j, spriteSize.y * i, 0));
+		//		child.transform.parent = transform;
+		//	}
+		//}
 
-		Destroy(childPrefab);
-		spriteRenderer.enabled = false;
+		//Destroy(childPrefab);
+		//spriteRenderer.enabled = false;
 
 	}
 }
