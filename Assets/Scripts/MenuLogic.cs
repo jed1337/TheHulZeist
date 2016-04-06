d8197a767a02bd3a0fe9ea936a71325ddb0570d4
Paolo Vittorio E. Merle <paolo.merle@hotmail.com>
2016-04-06 16:19:01 +0800

    Edited the Menus

--- Assets/Scripts/MenuLogic.cs
+++ Assets/Scripts/MenuLogic.cs
@@ -3,18 +3,11 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class MenuLogic : MonoBehaviour {
+	
+	public string levelToLoad;
 
-	public Texture backgroundTexture;
-	public Rect rect  = new Rect (Screen.width * .25f, Screen.height * .5f, Screen.width * .5f, Screen.height * .1f);
-	public string desc = "안녕하세요";
-	public string levelToLoad = "New Level 1";
-
-	void OnGUI(){
-
-		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), backgroundTexture);
-		if(GUI.Button (rect, desc)){
-
-			SceneManager.LoadScene(levelToLoad);
-		}
+	public void NewGame(){
+		
+		SceneManager.LoadScene(levelToLoad);
 	}
 }
