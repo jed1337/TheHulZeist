3c8203ff7c8980817c0d0e17d5cb229000668178
CactidSpoder <paolo.vittorio.merle@gmail.com>
2016-04-08 03:49:45 +0800

    camera mode: perspective, adjusted menu screens, player-treasure collision

--- Assets/Scripts/TreasureController.cs
+++ Assets/Scripts/TreasureController.cs
@@ -1,17 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class TreasureController : MonoBehaviour {
 
-	public LayerMask player;
+    private string colOtherLayer;
 
-	void OnCollisionEnter2D(Collision2D col) {
-		if (col.gameObject.layer == player.value) {
-			Debug.Log("PLAYER");
-			//Go to win screen
-		}
-		else {
-			Debug.Log(LayerMask.LayerToName(col.gameObject.layer));
-		}
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        colOtherLayer = LayerMask.LayerToName(col.gameObject.layer);
+
+        //If the contact is with a hostility
+
+        if (colOtherLayer == ConstantNames.PLAYER)
+        {
+            Destroy(col.gameObject);
+            SceneManager.LoadScene("Game Over");
+            Destroy(gameObject);    //Destroy the projectile
+        }
+    }
 }
